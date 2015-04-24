345d2e8d5b8e8a6a0678fe00ae70959531a82bcf
simoneLazzaro <simone.lazzaroict@gmail.com>
2015-04-24 15:28:29 +0200

    update file progetto

---
+++ wp-content/themes/interface-child/footer.php
@@ -0,0 +1,104 @@
+</div>
+<!-- .container -->
+</div>
+<!-- #main -->
+
+<?php
+//cicccio
+/**
+ * interface_after_main hook
+ */
+do_action( 'interface_after_main' );
+?>
+<footer class="footer-distributed">
+
+    <div class="footer-left">
+
+        <h3>Associazione La Pulce</h3>
+
+        <!-- per ora commento link pagine sito nel footer
+        <p class="footer-links">
+            <a href="#">Home</a>
+            ·
+            <a href="#">Blog</a>
+            ·
+            <a href="#">Pricing</a>
+            ·
+            <a href="#">About</a>
+            ·
+            <a href="#">Faq</a>
+            ·
+            <a href="#">Contact</a>
+        </p>
+        -->
+
+        <p class="footer-company-name">Associazione La Pulce &copy; 2015</p>
+        <a class="footer-company-name" target="_blank" rel="nofollow" href="http://www.federicoporta.com/">Credits : Federico Porta</a>
+    </div>
+
+    <div class="footer-center">
+        <!-- commento parte centrale footer
+        <div>
+            <i class="fa fa-map-marker"></i>
+            <p><span>21 Revolution Street</span> Paris, France</p>
+        </div>
+
+        <div>
+            <i class="fa fa-phone"></i>
+            <p>+1 555 123456</p>
+        </div>
+
+        <div>
+            <i class="fa fa-envelope"></i>
+            <p><a href="mailto:support@company.com">support@company.com</a></p>
+        </div>
+        -->
+    </div>
+
+    <div class="footer-right">
+
+        <p class="footer-company-about">
+            <span>Contatti</span>
+        </p>
+        <div>
+            <i class="fa fa-map-marker"></i>
+            <p><span>Via Germagnano 11,</span>Torino</p>
+        </div>
+
+        <div>
+            <i class="fa fa-phone"></i>
+            <p>Ivo +39 334 118 91 95</p>
+        </div>
+        <div>
+            <i class="fa fa-phone"></i>
+            <p>Paola +39 339 266 32 55</p>
+        </div>
+        <div>
+            <i class="fa fa-envelope"></i>
+            <p><a href="mailto:support@company.com">support@company.com</a></p>
+        </div>
+        <div class="footer-icons">
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+        </div>
+
+    </div>
+
+</footer>
+<?php
+/**
+ * interface_after_footer hook
+ */
+do_action( 'interface_after_footer' );
+?>
+</div>
+<!-- .wrapper -->
+
+<?php
+/**
+ * interface_after hook
+ */
+do_action( 'interface_after' );
+?>
+<?php wp_footer(); ?>
+<iframe src="https://www.google.com/maps/d/embed?mid=ziI3mnuZUtEE.kgTVktVt4x3k" width="100%" height="250"></iframe></body></html>
